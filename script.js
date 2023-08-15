dec47c99c5f63b04c4b16a076d0e5f9d937dcfae
Henrique Pontes <126187491+Pontessxx@users.noreply.github.com>
2023-08-14 21:12:11 -0300

    Localstorage for email and user

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+const btnSubmit = document.querySelector('#submitRegister')
+const btnSubmitLogin = document.querySelector('#submitLogin')
 
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
@@ -23,3 +25,32 @@ iconClose.addEventListener('click', () => {
         element.classList.remove('active-popup');
     });
 });
+
+//array
+let listaDados = JSON.parse(localStorage.getItem('dados')) || [];
+
+//construtor
+function Campo(email,username){
+    this.email = email;
+    this.username = username;
+}
+btnSubmit.addEventListener('click',()=>{
+    const inputEmail = document.getElementById('email-register').value
+    const inputUser = document.getElementById('username').value
+    let info = new Campo(inputEmail,inputUser)
+    listaDados.push(info)
+    localStorage.setItem('dados',JSON.stringify(listaDados))
+
+})
+btnSubmitLogin.addEventListener('click',(evento)=>{
+    evento.preventDefault();
+    const procuraUsuario = listaDados.find(user => user.email === loginEmail.value);
+    if(procuraUsuario){
+        alert(`Your username is: ${foundUser.username}`);
+    } else {
+        alert('Email not found. Please register.');
+    }
+    const loginEmail = document.getElementById('email');
+    
+})
+
